9465ab06cf311dfef69c6c3743ceece9d6270412
arantesandre <arantes.andreluiz@gmail.com>
2024-12-26 23:53:34 -0300

    conexao

--- src/database/ConexaoMySql.js
+++ src/database/ConexaoMySql.js
@@ -1,19 +1,23 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dbConfig = {
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "senai",
-  database: "banco_dados_briks",
+  host: process.env.MYSQLHOST || "localhost",
+  port: process.env.MYSQLPORT || "3306",
+  user: process.env.MYSQLUSER || "root",
+  password: process.env.MYSQLPASSWORD || "senai",
+  database: process.env.MYSQLDATABASE || "banco_dados_briks",
 };
 
 class ConexaoMySql {
+  static conexao = null;
+
   async getConexao() {
     if (!ConexaoMySql.conexao) {
       ConexaoMySql.conexao = await mysql.createConnection(dbConfig);
     }
-
     return ConexaoMySql.conexao;
   }
 }
